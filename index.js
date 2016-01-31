4f06f30daeab88b4903f0f7bb80d0d50aea80ae8
Elana Kopelevich <ekopelevich@gmail.com>
2016-01-31 09:40:13 -0700

    added a Shiblyism

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ module.exports = {
       'Dude! Fresh Starbucks food!!!',
       'Have you guys ever thought about the implications of strong AI coming to fruition in our lifetime? Such superintelligence would not be just another technological development; it would be the most important invention ever made, and would lead to explosive progress in all scientific and technological fields, as the superintelligence would conduct research with superhuman efficiency! To the extent that ethics is a cognitive pursuit, a superintelligence could also easily surpass humans in the quality of its moral thinking!!! Fuck!',
       'We\'re all wizards. No, seriously.',
-      'Right on.'
+      'Right on.',
+      'Check out these comics!'
     ]
     var randomIndex = Math.ceil(Math.random() *  shibleyIsms.length-1)
     return shibleyIsms[randomIndex]
